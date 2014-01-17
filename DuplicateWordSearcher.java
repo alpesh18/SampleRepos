ec5b03738ac0b1752237f4c99e6b9c8fdcf19354
alpesh18 <alpeshrthd@gmail.com>
2014-01-17 12:18:47 +0530

    Duplicate Word Searcher Program
    Duplicate Word Searcher Program

---
+++ DuplicateWordSearcher.java
@@ -0,0 +1,27 @@
+package com.java.interview.examples;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Sample class for searching duplicate words in a string
+ * 
+ * @author Alpesh Rathod
+ * 
+ */
+public class DuplicateWordSearcher {
+	public static void main(String[] args) {
+
+		String text = "a r b k c d se f g a d f s s f d s ft gh f ws w f v x s g h d h j j k f sd j e wed a d f";
+
+		List<String> list = Arrays.asList(text.split(" "));
+
+		Set<String> uniqueWords = new HashSet<String>(list);
+		for (String word : uniqueWords) {
+			System.out.println(word + ": " + Collections.frequency(list, word));
+		}
+	}
+}
